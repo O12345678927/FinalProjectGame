3633106f79e29261e6353b0cf5e035247b278b59
Matthew <Mcdonald.Matthew.M@hotmail.com>
2019-12-08 23:15:25 -0500

    [Matthew] 08/12/2019
    [Add]
    -Mountain
    -Teleporters
    [Mod]
    -Teleporter Script

--- FinalProject(Unity)/Assets/Scripts/MISC/Teleport.cs
+++ FinalProject(Unity)/Assets/Scripts/MISC/Teleport.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Teleport : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Vector3 offset;
+    public Transform destination;
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D toPort)
     {
-        
+        toPort.transform.position = destination.position + offset;
+        Debug.Log(toPort.transform.position);
     }
 }
