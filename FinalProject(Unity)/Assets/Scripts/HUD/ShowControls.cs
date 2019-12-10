2b21f40ae8f3e6ef538fa498897f03703d6a3eec
Chloe <Baker>
2019-12-10 12:51:06 -0500

    [Chloe] 10/12/2019
    [add]
    - added a start screen (put the code in ShowControls script)

--- FinalProject(Unity)/Assets/Scripts/HUD/ShowControls.cs
+++ FinalProject(Unity)/Assets/Scripts/HUD/ShowControls.cs
@@ -8,15 +8,34 @@ public class ShowControls : MonoBehaviour
     public Image controlsBackground;
     public Image controls;
 
+    private Image startScreen;
+    private Image start;
+
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("DisableText", 10f); // Close the controls window after 10 seconds
+        startScreen = GameObject.Find("StartScreen").GetComponentInChildren<Image>();
+        start = GameObject.Find("Start").GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            CloseStart();
+            Invoke("DisableText", 10f); // Close the controls window after 10 seconds
+        }
     }
 
     void DisableText()
     {
         controls.enabled = false;
         controlsBackground.enabled = false;
+    }
+
+    void CloseStart()
+    {
+        startScreen.enabled = false;
+        start.enabled = false;
     }
 }
